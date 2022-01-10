a2cf07cd53886290bded1d1187e0d4a74a196eca
blackadder-git <sauterelle.web@gmail.com>
2022-01-10 14:42:27 +0000

    wdd330 initial files

---
+++ wdd330/js/main.js
@@ -0,0 +1,22 @@
+function addWeeks() {
+    let weekList = document.querySelector("#weekList");
+    const weeks = [
+        {
+          label: "Week 1",
+          url: "week1/index.html"
+        },
+        {
+            label: "Week 2",
+            url: "week2/index.html"
+        }
+    ]
+
+    // loop weeks
+    weeks.forEach ( link => {
+        let week = document.createElement('li');
+        week.innerHTML = `<a href="${link.url}">${link.label}</a>`;
+        weekList.append(week);
+    });
+}
+
+addWeeks();
